a275c1ccc99b4d9268940b6fc392a66f9ddcf545
Biswambar Pradhan <biswambarp43@gmail.com>
2022-06-17 12:50:10 +0530

    bubbleSort

--- src/main/java/Sorting.java
+++ src/main/java/Sorting.java
@@ -43,4 +43,20 @@ public class Sorting {
     }
     return nums;
   }
+
+  public static int[] bubbleSort(int[] nums) {
+    if (nums.length < 2) return nums;
+
+    for (int i = 0; i < nums.length; i++) {
+      for (int j = i + 1; j < nums.length; j++) {
+
+        if (nums[i] > nums[j]) {
+          int temp = nums[i];
+          nums[i] = nums[j];
+          nums[j] = temp;
+        }
+      }
+    }
+    return nums;
+  }
 }
